0fe2fb9f66f434b673506e05ebddf0d6115316c3
Oliver <hnsxxscyx@gmail.com>
2022-10-13 22:12:31 +0800

    feat: add custom MailServiceProvider

---
+++ src/MailServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MultipleMailerDriver;
+
+use Illuminate\Mail\MailServiceProvider as BaseMailServiceProvider;
+
+class MailServiceProvider extends BaseMailServiceProvider
+{
+    protected function registerIlluminateMailer()
+    {
+        $this->app->singleton('mail.manager', function ($app) {
+            return new MailManager($app);
+        });
+
+        $this->app->bind('mailer', function ($app) {
+            return $app->make('mail.manager')->mailer();
+        });
+    }
+}
